f5ab3b277bb15a8f1103e84deffc2e71835e0825
Daniel Schneider <daschne8@gmail.com>
2019-01-13 19:37:54 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+Object playlist = {
+  daftPunk:"Digital Love",
+};
+function updatePlaylist(playlist, name, title){
+  playlist[name] = title;
+  return playlist
+}
+function removeFromPlaylist(playlist,artist){
+  delete playlist.artist;
+}
